5fb65b8fc9fa7f1b8676099340fb40a8384c9e34
CoachGaming <dovantung1000@gmail.com>
2023-06-06 10:17:19 +0700

    change layout main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,285 +1,90 @@
 import React from "react";
 import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, Card, Row, Col } from "antd";
-const { Header, Sider, Content } = Layout;
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme ,Row , Col ,Card} from "antd";
 
-export default function Home() {
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <Layout className="layout">
-      <Sider trigger={null} style={{position:'fixed' , height: '100vh'}}>
-        <div className="logo" />
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="demo-logo-vertical" />
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "Danh sach nhan vien",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "Kho",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
-            },
-          ]}
+          defaultSelectedKeys={["4"]}
+          items={items}
         />
       </Sider>
-      <Layout className="site-layout">
-        <Header
-          className="site-layout-background"
-        >
-          
-        </Header>
-        <Content
-          style={{
-            marginLeft : 210,
-            margin: "20px 16px",
-            marginTop:70
-          }}
-        >
-          <Row gutter={16}>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            <Col span={6} style={{marginTop:10}}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
-                  />
-                }
-              >
-                <h3>ID :</h3>
-                <h3>Name :</h3>
-              </Card>
-            </Col>
-            
-          </Row>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            style={{
+              padding: '10px 16px',
+              textAlign: "center",
+              background: colorBgContainer,
+            }}
+          >
+            <Row gutter={16}>
+              <Col span={6} style={{marginTop:10}}>
+                <Card
+                  hoverable
+                  cover={
+                    <img
+                      alt="example"
+                      src="https://anhdep123.com/top-300-hinh-nen-anh-thien-nhien-4k-chat-luong-cao/hinh-nen-thien-nhien-4k/"
+                    />
+                  }
+                >
+                  <h3>ID :</h3>
+                  <h3>Name :</h3>
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </Content>
       </Layout>
     </Layout>
   );
-}
+};
+
+export default App;
